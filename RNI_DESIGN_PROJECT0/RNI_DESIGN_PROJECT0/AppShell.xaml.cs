d5ea18100d5cb63d3d42690da345c688e36aad58
Mateus Kelly <mkelly20@umd.edu>
2021-06-08 10:32:09 -0400

    hello

---
+++ RNI_DESIGN_PROJECT0/RNI_DESIGN_PROJECT0/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace RNI_DESIGN_PROJECT0
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
