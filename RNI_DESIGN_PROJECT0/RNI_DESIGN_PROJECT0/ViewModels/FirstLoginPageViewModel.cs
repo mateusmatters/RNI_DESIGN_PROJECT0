52e62050a868dfb2f14299b6ba4a4c4e7433c845
Mateus Kelly <mkelly20@umd.edu>
2021-06-10 16:42:06 -0400

    changed login picture, still need to work on login functionalities

---
+++ RNI_DESIGN_PROJECT0/RNI_DESIGN_PROJECT0/ViewModels/FirstLoginPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RNI_DESIGN_PROJECT0.ViewModels
+{
+    public class FirstLoginPageViewModel : BindableObject
+    {
+        string inputedUserName = "Bob";
+
+        public FirstLoginPageViewModel() {
+            UserEntry = new Command(OnUserEntry);
+        }
+
+        public string InputedUserName {
+            get => inputedUserName;
+            set {
+                if (value == inputedUserName) {
+                    return;
+                }
+                inputedUserName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand UserEntry;
+        void OnUserEntry() {
+            inputedUserName = "Bob";
+        }
+    }
+}
--- RNI_DESIGN_PROJECT0/RNI_DESIGN_PROJECT0/Views/DailyActivitiesPage.xaml.cs
+++ RNI_DESIGN_PROJECT0/RNI_DESIGN_PROJECT0/Views/DailyActivitiesPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using RNI_DESIGN_PROJECT0.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace RNI_DESIGN_PROJECT0.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class FirstLoginPage : ContentPage
+    public partial class DailyActivitiesPage : ContentPage
     {
-        public FirstLoginPage()
+        public DailyActivitiesPage()
         {
             InitializeComponent();
         }
